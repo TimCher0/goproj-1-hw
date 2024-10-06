f162e89f5c3ef6030d48f9dcf387586b77027447
Timofey Chernyavskiy <m2411526@edu.misis.ru>
2024-10-07 00:57:20 +0300

    Add files via upload

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"livecode/internal/pkg/storage"
+	"log"
+)
+
+func main() {
+	st, err := storage.NewStorage()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+
+	st.Set("key2", "222")
+	rlt2 := st.GetKind("key2")
+	fmt.Println(rlt2)
+}
---
+++ internal/pkg/storage/storage.go
@@ -0,0 +1,47 @@
+package storage
+
+import (
+	"strconv"
+
+	"go.uber.org/zap"
+)
+
+type Storage struct {
+	ins    map[string]string
+	logger *zap.Logger
+}
+
+func NewStorage() (Storage, error) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return Storage{}, err
+	}
+	defer logger.Sync()
+	logger.Info("new storage created")
+	return Storage{
+		ins: make(map[string]string),
+	}, nil
+}
+
+func (rf Storage) Set(key, value string) {
+	rf.ins[key] = value
+	rf.logger.Info("new key set")
+	rf.logger.Sync()
+}
+
+func (rf Storage) Get(key string) *string {
+	res, tr := rf.ins[key]
+	if !tr {
+		return nil
+	}
+	rf.logger.Info("key obtained", zap.String("value", res))
+	rf.logger.Sync()
+	return &res
+}
+func (rf Storage) GetKind(key string) string {
+	_, tr2 := strconv.Atoi(rf.ins[key])
+	if tr2 == nil {
+		return "d (key of type integer)"
+	}
+	return "c (key of type string)"
+}
