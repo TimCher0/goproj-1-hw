beee5e8932d063eef410b40c403b76e8e7b5cdfd
Timofey Chernyavskiy <m2411526@edu.misis.ru>
2024-10-08 11:33:22 +0300

    Add files via upload

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,11 @@ func main() {
 		log.Fatalf("can't initialize zap logger: %v", err)
 	}
 
-	st.Set("key2", "222")
-	rlt2 := st.Get("key2")
-	fmt.Println(*rlt2)
+	st.Set("key1", "11")
+	st.Set("key2", "two")
+	st.Set("key3", "")
+	rlt1 := st.GetKind("key1")
+	rlt2 := st.GetKind("key2")
+	rlt3 := st.Get("key2")
+	fmt.Println(rlt1, rlt2, *rlt3)
 }
